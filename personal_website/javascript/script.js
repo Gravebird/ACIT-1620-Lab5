22e4dee1aae334149ac828df492b0f16192d4b4e
Gravebird <71402180+Gravebird@users.noreply.github.com>
2020-11-24 17:16:13 -0800

    Create script.js

---
+++ personal_website/javascript/script.js
@@ -0,0 +1,5 @@
+
+var courseList = new Array();
+courseList[0] = {code: "ACIT 1620", name: "Web Fundamental Technologies"};
+courseList[1] = {code: "ACIT 1515", name: "Scripting for IT"};
+courseList[2] = {code: "ACIT 1630", name: "Database Systems"};
