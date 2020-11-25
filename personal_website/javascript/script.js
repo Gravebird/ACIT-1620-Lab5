8f0671b6a6b3a385e53e435ebfc81a4dc00d567f
Gravebird <71402180+Gravebird@users.noreply.github.com>
2020-11-24 18:18:34 -0800

    Remove function call to start script

--- personal_website/javascript/script.js
+++ personal_website/javascript/script.js
@@ -4,10 +4,6 @@ courseList[0] = {code: "ACIT 1620", name: "Web Fundamental Technologies"};
 courseList[1] = {code: "ACIT 1515", name: "Scripting for IT"};
 courseList[2] = {code: "ACIT 1630", name: "Database Systems"};
 
-askUserForCourseCode();
-
-
-
 function getInput() {
   var code = "invalid";
   while (!isInt(code)) {
